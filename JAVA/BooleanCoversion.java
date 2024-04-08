53b05697561e0a1d9b70a92060c0941244cb92d3
322mnisi <322mnisi@gmail.com>
2024-04-08 21:33:18 +0200

    commit

--- JAVA/BooleanCoversion.java
+++ JAVA/BooleanCoversion.java
@@ -1,22 +1,20 @@
 import java.util.Scanner;
 public class BooleanCoversion {
-//	public static boolean stringToBoolean(String str) {
-//		return Boolean.parseBoolean(str);
-	
 	public static void main(String args[]) {
 
-	//Scanner switch = new Scanner(System.in);
+	Scanner lightStatus = new Scanner(System.in);
 	System.out.println("What is the indicator showing on the dashboard, it should either be a 1 or 0?");
-	String str = "yes";
-	String str = "true";
-    String str = "false";
-    //String b = switch.nextBoolean();
-	//if (b = 1);
-	System.out.println(stringToBoolean(lightsOn));
-	//else if (b = 0);
-	System.out.println(stringToBoolean(lightsOff));
-    //else 
 
-	//System.out.println("You have entered an invalid input, please enter 1 or 0 as shown on the dashboard");
-	}
+  int n = lightStatus.nextInt();
+
+
+	if (n == 1){
+	System.out.println("Lights are on");
+}
+    if (n == 0){
+	System.out.println("Lights are off");
+}
+    else if (n != 0 && n != 1){
+    System.out.println("You have entered an invalid input, please enter 1 or 0 as shown on the dashboard");}
+}
 }
--- JAVA/Concatenation.java
+++ JAVA/Concatenation.java
@@ -2,15 +2,12 @@ import java.util.Scanner;
 public class Concatenation {
 	public static void main(String args[]) {
 
-//char s = input.charAT(0);
-//char n = input.charAT(0);
-
 	Scanner song = new Scanner(System.in);
-	System.out.println("Please type in the first line in Sponge Bob Square pant song.");
-	String s = song.next();
+	System.out.println("Please type in the first line in Sponge Bob Square pants song.");
+	String s = song.nextLine();
 	Scanner song1 = new Scanner(System.in);
-	System.out.println("Please type in the second line in Sponge Bob Square pant song.");
-	String n = song1.next();
-	System.out.print(s + "\n"+ n);
+	System.out.println("Please type in the second line in Sponge Bob Square pants song.");
+	String n = song1.nextLine();
+	System.out.println(s + "\n" + n);
 	}
 }
