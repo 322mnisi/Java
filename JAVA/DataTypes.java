81474b262adc4786c3c83bed926beb2526e4f523
322mnisi <322mnisi@gmail.com>
2024-04-09 20:20:07 +0200

    commit

---
+++ JAVA/DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes{
+    public static void main(String [] args){
+        int myInt = 1;
+        boolean myBoolean = true;
+        double myDouble =  333.56;
+ 	   float myFloat =  333.56F;
+        byte  myByte = 4;
+ 	   long myLong = 24;
+        short myShort = 7;
+        char myChar = 'A';
+ 	   System.out.println(myDouble);
+    }
+}
