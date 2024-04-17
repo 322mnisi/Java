cf47d02cebf6f52e94784cc70bd70c07c8b89d21
322mnisi <322mnisi@gmail.com>
2024-04-17 08:40:04 +0200

    commit

---
+++ JAVA/ArithmethicExamples.java
@@ -0,0 +1,13 @@
+public class ArithmethicExamples {
+	public static void main(String [] args){
+
+	int add = 4;
+	int newadd = add ++ ;
+	//int subtract = add -- ;
+	//int newsub = subtract;
+
+
+		System.out.println(newadd);
+
+}
+}
---
+++ JAVA/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String [] args){
+
+		int n = 1;
+		boolean b = true;
+		double d = 	1.1D;
+		char c = 'u';
+        String s = "Musa";
+
+        System.out.println(b);
+	}
+}
