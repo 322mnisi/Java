59f712d66ed838de2e1eb47b0f71936a0af0b1b9
322mnisi <322mnisi@gmail.com>
2024-05-03 14:06:38 +0200

    commit

--- JAVA/NumberValidator.java
+++ JAVA/NumberValidator.java
@@ -1,12 +1,27 @@
 public class NumberValidator {
-	public static void numberValidator(String distance){
+	public static Boolean numberValidator(String distance){
 
-		double distanceToStore = Double.parseDouble(distance);
+	// 	double distanceToStore = Double.parseDouble(distance);
 
-		 if (distanceToStore < 0 || distanceToStore > 32000){
-		 	System.out.println("Distance is not valid!!! Exiting");
-            System.exit(0);
-		 }
-            else {}
+	// 	 if (distanceToStore < 0 || distanceToStore > 32000){
+	// 	 	System.out.println("Distance is not valid!!! Exiting");
+    //         System.exit(0);
+	// 	 }
+    //         else {}
+	// 	}
+	// }
+
+
+if (distance != null && !distance.isEmpty()){
+	for (char c : distance.toCharArray()){
+		if (!Character.isDigit(c)){
+			return false;
 		}
 	}
+	return true;
+		}else {
+			return false;
+		}
+	}
+}
+
